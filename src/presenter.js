111b370153bb028a058d7f1862ca8fb59bf3972b
ela251643010 <daniela.condori.b@ucb.edu.bo>
2025-09-10 11:25:03 -0400

    feat: se muestra y captura la fecha de ingreso

--- src/presenter.js
+++ src/presenter.js
@@ -3,13 +3,22 @@ import sumar from "./sumador";
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
-
+/*
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
+  const firstNumber = Number.parseInt(first.  value);
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
-});
+});*/
+ const fechaIngreso = document.querySelector("#fecha-ingresada");
+ const fechaForm = document.querySelector("#parqueo-form");
+ const div = document.querySelector("#resultado-div");
+ const boton = document.querySelector("#Enviar-Button");
+ fechaForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const [anio,mes,dia] = fechaIngreso.value.split("-");
+  const fechaOficial = `${dia}/${mes}/${anio}`;
+  div.innerHTML = "<p>" +"La fecha de ingreso es:"  + fechaOficial + "</p>";
+ });
