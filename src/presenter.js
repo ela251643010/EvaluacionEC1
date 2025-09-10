e686f6994b8df8e5ef496be436b5537bec94ebd6
ela251643010 <daniela.condori.b@ucb.edu.bo>
2025-09-10 11:33:52 -0400

    feat:ingresa y muestra la hora de salida

--- src/presenter.js
+++ src/presenter.js
@@ -14,6 +14,8 @@ form.addEventListener("submit", (event) => {
 });*/
  const fechaIngreso = document.querySelector("#fecha-ingresada");
  const fechaSalida = document.querySelector("#fecha-salida");
+ const horaIngreso = document.querySelector("#hora-ingreso");
+ const horaSalida = document.querySelector("#hora-salida");
  const fechaForm = document.querySelector("#parqueo-form");
  const div = document.querySelector("#resultado-div");
  const boton = document.querySelector("#Enviar-Button");
@@ -23,5 +25,6 @@ form.addEventListener("submit", (event) => {
   const fechaOficial = `${dia}/${mes}/${anio}`;
   const [anio2,mes2,dia2] = fechaSalida.value.split("-");
   const fechaOficial2 = `${dia2}/${mes2}/${anio2}`;
-  div.innerHTML = "<p>" +"La fecha de ingreso es:"  + fechaOficial + "</p>"+ "<br>" + "<p>" +"La fecha de salida es:"  + fechaOficial2 + "</p>";
+  div.innerHTML = "<p>" +"La fecha de ingreso es:"  + fechaOficial + "</p>"+ "<br>" + "<p>" +"La fecha de salida es:"  + fechaOficial2 + "</p>"
+  + "<br>" + "<p>" +"La hora de ingreso es:"  + horaIngreso.value + "</p>" + "<br>" + "<p>" +"La hora de salida es:"  + horaSalida.value + "</p>  ";
  });
